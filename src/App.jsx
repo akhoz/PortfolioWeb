bc9ebf23f65b58924e00b5f5fdd387c92a23cfbd
akhoz <adrian25092005@gmail.com>
2024-03-21 08:21:22 -0600

    Fixing error releated to images

--- src/App.jsx
+++ src/App.jsx
@@ -37,9 +37,9 @@ function App() {
                 <IoIosArrowDropdownCircle className={`hidden md:block text-6xl animate-bounce my-10 md:my-16 ${isLightMode ? "text-lightGradientInit" : "text-darkGradientInit"}`} />
 
                 <div className='flex flex-grow flex-col justify-center text-center space-y-5 mb-5 px-3 md:space-y-0 md:space-x-5 md:mb-24 md:flex-row md:w-fit'>
-                    <CustomCard  imgSrc='wallhaven-3lyrvy_1920x1080.png' alt='AboutMeIMG' title='About Me' description='Here you can find general information about me.' />
-                    <CustomCard  imgSrc='wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
-                    <CustomCard  imgSrc='wallhaven-3lyrvy_1920x1080.png' alt='SkillsIMG' title='Skills' description='Discover my skills in tech and math.' />
+                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='AboutMeIMG' title='About Me' description='Here you can find general information about me.' />
+                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
+                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='SkillsIMG' title='Skills' description='Discover my skills in tech and math.' />
                 </div>
 
                 <div className='flex flex-row mt-10 mb-4 opacity-50 space-x-2 items-center justify-center'>
--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -5,7 +5,7 @@ function ProfilePicture() {
 
     return (
         <div className={`w-48 h-48 rounded-full overflow-hidden flex items-center justify-center ${isLightMode ? "bg-gradient-to-r from-lightGradientInit to-lightGradientEnd" : "bg-gradient-to-r from-darkGradientInit to-darkGradientEnd"}`}>
-            <img src="/images/pfp.jpg" alt="Profile Picture" className="w-11/12 h-11/12 object-cover rounded-full"/>
+            <img src="./images/pfp.jpg" alt="Profile Picture" className="w-11/12 h-11/12 object-cover rounded-full"/>
         </div>
     );
 }
