f174471959319805d0c2b4f81fb32a0503b800e8
akhoz <adrian25092005@gmail.com>
2024-03-21 23:20:53 -0600

    Improving Projects Page responsive design

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,37 +7,29 @@ function Projects() {
     const { isLightMode } = useTheme();
 
     return (
-        <div className="flex flex-grow flex-col items-center md:items-left mt-36">
-            <div>
-                <div className={`${isLightMode ? 'text-black' : 'text-white'} flex flex-row text-2xl md:text-4xl font-bold justify-start items-center`}>
+        <div className="flex flex-grow flex-col items-center lg:items-left mt-36 mb-5">
+                <div className={`${isLightMode ? 'text-black' : 'text-white'} flex flex-row text-2xl md:text-4xl font-bold justify-start items-center lg:w-screen lg:max-w-screen-lg xl:max-w-screen-xl`}>
                     <span>Hall of Fame</span>
                     <PiCrownSimpleFill className='text-3xl md:text-5xl ml-2' />
                 </div>
 
-                <div className={`flex flex-wrap justify-center items-center space-x-5 space-y-5 my-5 w-80 md:max-w-screen-xl md:w-screen`}>
-                <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
-                <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
-                <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
-                </div>
-
-                <div className={`${isLightMode ? 'text-black' : 'text-white'} flex flex-row text-2xl md:text-4xl font-bold justify-start items-center mt-12`}>
-                    <span>My Projects</span>
-                    <IoIosPaper className='text-2xl md:text-4xl ml-2' />
-                </div>
-
-                <div className={`flex flex-wrap justify-center items-center text-center space-x-5 space-y-5 my-5 w-full md:max-w-screen-xl md:w-screen`}>
-
-                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
-                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
-                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
-
+                <div className={`flex flex-col justify-center items-center space-y-5 my-3 w-80 md:max-w-screen-lg md:w-screen lg:flex-row lg:space-x-5 lg:space-y-0  lg:max-w-screen-lg xl:max-w-screen-xl`}>
                     <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
                     <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
                     <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
                 </div>
 
+                <div className={`${isLightMode ? 'text-black' : 'text-white'} flex flex-row text-2xl mt-5 md:text-4xl font-bold justify-start items-center lg:w-screen lg:max-w-screen-lg xl:max-w-screen-xl`}>
+                    <span>Personal Projects</span>
+                    <IoIosPaper className='text-3xl md:text-5xl ml-2' />
+                </div>
 
-            </div>
+                <div className={`flex flex-col justify-center items-center space-y-5 my-3 w-80 md:max-w-screen-lg md:w-screen lg:flex-row lg:space-x-5 lg:space-y-0  lg:max-w-screen-lg xl:max-w-screen-xl`}>
+                    
+                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
+                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
+                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
+                </div>
         </div>
     );
 }
