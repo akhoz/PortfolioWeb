ed44a28a492c0c1abdfb979a5cc427d3ee990680
akhoz <adrian25092005@gmail.com>
2024-03-21 23:42:34 -0600

    Improving Projects Page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,8 @@
 import CustomCard from "../components/CustomCard.jsx";
+import OutlineButton from '../components/OutlineButton.jsx';
 import { useTheme } from "../contexts/ThemeContext.jsx";
 import { PiCrownSimpleFill } from "react-icons/pi";
+import { FaStar } from "react-icons/fa";
 import { IoIosPaper } from "react-icons/io";
 
 function Projects() {
@@ -21,6 +23,18 @@ function Projects() {
 
                 <div className={`${isLightMode ? 'text-black' : 'text-white'} flex flex-row text-2xl mt-5 md:text-4xl font-bold justify-start items-center lg:w-screen lg:max-w-screen-lg xl:max-w-screen-xl`}>
                     <span>Personal Projects</span>
+                    <FaStar className='text-3xl md:text-5xl ml-2' />
+                </div>
+
+                <div className={`flex flex-col justify-center items-center space-y-5 my-3 w-80 md:max-w-screen-lg md:w-screen lg:flex-row lg:space-x-5 lg:space-y-0  lg:max-w-screen-lg xl:max-w-screen-xl`}>
+                    
+                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
+                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
+                    <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
+                </div>
+
+                <div className={`${isLightMode ? 'text-black' : 'text-white'} flex flex-row text-2xl mt-5 md:text-4xl font-bold justify-start items-center lg:w-screen lg:max-w-screen-lg xl:max-w-screen-xl`}>
+                    <span>Academic Projects</span>
                     <IoIosPaper className='text-3xl md:text-5xl ml-2' />
                 </div>
 
@@ -29,6 +43,10 @@ function Projects() {
                     <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
                     <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
                     <CustomCard  imgSrc='/wallhaven-3lyrvy_1920x1080.png' alt='ProjectIMG' title='Projects' description='Explore the projects I&apos;ve developed in this section.' />
+                </div>
+
+                <div className='mt-5 my-3'>
+                    <OutlineButton text="Find the rest of my projects on GitHub" link="https://github.com/akhoz"/>
                 </div>
         </div>
     );
