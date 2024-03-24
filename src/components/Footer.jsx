8337147f33e887d8b1af09aed3279bf24e125eca
akhoz <adrian25092005@gmail.com>
2024-03-24 13:51:57 -0600

    Improving Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     const { isLightMode } = useTheme();
 
     return (
-        <footer className={`flex justify-center items-center md:h-10 ${isLightMode ? "bg-lightGray" : "bg-darkBlue"}`}>
+        <footer className={`flex justify-center items-center py-3 md:h-10 ${isLightMode ? "bg-lightGray" : "bg-darkBlue"}`}>
             <p className={`${isLightMode ? 'text-black' : 'text-white'} opacity-70`}>
                 &copy; 2023 AkhozDev
             </p>
