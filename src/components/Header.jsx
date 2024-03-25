2a51b56e9dead01c209f8880d04c62e0d8a73f31
akhoz <adrian25092005@gmail.com>
2024-03-25 11:51:14 -0600

    Implementing new Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 
-import { Button, Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
 import { useTheme } from '../contexts/ThemeContext.jsx';
 import ThemeToggle from './ThemeToggle.jsx';
-import { BsList} from 'react-icons/bs';
 
 function Header() {
     const { isLightMode, toggleTheme } = useTheme();
@@ -13,60 +11,17 @@ function Header() {
     }
 
     return (
-        <>
-            {['md'].map((expand) => (
-                <Navbar
-                    key={expand}
-                    expand={expand}
-                    className={`mb-3 fixed top-0 left-0 right-0 z-10 p-4 transition-all duration-300 ${
-                        isLightMode
-                            ? 'bg-lightGray backdrop-blur-md backdrop-filter-saturate opacity-80'
-                            : 'bg-darkBlue backdrop-blur-md backdrop-filter-saturate opacity-80'
-                    }`}
-                >
-                    <Container fluid>
-                        <Navbar.Brand href="#" className={`${isLightMode ? 'text-black' : 'text-white'}`}>
-                            <Link to ='/PortfolioWeb/' onClick={handleButtonClick}>
-                                AkhozDev
-                            </Link>
-                        </Navbar.Brand>
-                        <Navbar.Toggle
-                            aria-controls={`offcanvasNavbar-expand-${expand}`}
-                            className={`${isLightMode ? 'text-black' : 'text-white'}`}
-                        >
-                            <BsList />
-                        </Navbar.Toggle>
-                        <Navbar.Offcanvas
-                            id={`offcanvasNavbar-expand-${expand}`}
-                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                            placement="end"
-                            className={`transition-all duration-300 ${isLightMode ? 'bg-lightGray' : 'bg-darkBlue'}`}
-                        >
-                            <Offcanvas.Header closeButton >
-                                <Offcanvas.Title
-                                    id={`offcanvasNavbarLabel-expand-${expand}`}
-                                    className={`${isLightMode ? 'text-black' : 'text-white'}`}
-                                >
-                                    AkhozDev
-                                </Offcanvas.Title>
-                            </Offcanvas.Header>
-                            <Offcanvas.Body className="items-center">
-                                <hr className={`border-t-2 mx-auto mb-4 ${isLightMode ? "border-lightGradientInit" : "border-darkGradientInit"}`}/>
-                                <Nav className={`justify-content-end flex-grow-1 pe-3`}>
-                                    <Nav.Link href="" className={`${isLightMode ? 'text-black' : 'text-white'}`} onClick={handleButtonClick}>
-                                    <Link to ='/PortfolioWeb/' onClick={handleButtonClick}>
-
-                                        Home
-                                    </Link>
-                                    </Nav.Link>
-                                </Nav>
-                                <ThemeToggle />
-                            </Offcanvas.Body>
-                        </Navbar.Offcanvas>
-                    </Container>
-                </Navbar>
-            ))}
-        </>
+        <div className={`flex flex-row justify-between items-center mb-3 fixed top-0 left-0 right-0 z-10 p-4 transition-all duration-300 backdrop-blur-sm ${isLightMode ? 'bg-lightGray' : 'bg-darkBlue'}`}>
+            <Link to ='/PortfolioWeb/' onClick={handleButtonClick} className={`${isLightMode ? 'text-black' : 'text-white'}`}>
+                AkhozDev
+            </Link>
+            <div className='flex flex-row space-x-2 md:space-x-5 items-center'>
+                <Link to ='/PortfolioWeb/' onClick={handleButtonClick} className={`${isLightMode ? 'text-black' : 'text-white'}`}>
+                    Home
+                </Link>
+                <ThemeToggle />
+            </div>  
+        </div>
     );
 }
 
